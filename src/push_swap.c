62686d19becb4095193cb3e6a138e4dc26fe415a
QuiteHollow <robert123horbach@gmail.com>
2023-04-18 18:09:55 +0200

    added header and working stack_a list

---
+++ src/push_swap.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   push_swap.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rhorbach <rhorbach@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/04/17 14:46:51 by rhorbach      #+#    #+#                 */
+/*   Updated: 2023/04/18 17:10:00 by rhorbach      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+void list_print(t_list *list)
+{
+	while (list != NULL)
+	{
+		printf("%s\n", list->content);
+		list = list->next;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	t_list	*stack_a;
+	t_list	*stack_b;
+	t_list	*new_val;
+
+	stack_a = NULL;
+	stack_b = NULL;
+	// if (argc == 0)
+	// 	return (error);
+	// //parsing
+	i = 1;
+	while (i < argc)
+	{
+		printf("adding: %s\n", argv[i]);
+		new_val = ft_lstnew(argv[i]);
+		if (new_val == NULL)
+		{
+			ft_lstclear(&stack_a, NULL);
+			return (0);
+		}
+		ft_lstadd_back(&stack_a, new_val);
+		i++;
+	}
+
+	//functions for all operations
+	list_print(stack_a);
+	// printf("argc: %i\n", argc);
+}
---
+++ src/push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   push_swap.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rhorbach <rhorbach@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/04/18 15:15:16 by rhorbach      #+#    #+#                 */
+/*   Updated: 2023/04/18 15:16:22 by rhorbach      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <errno.h>
+# include <fcntl.h>
+# include "libft.h"
+
+#endif
